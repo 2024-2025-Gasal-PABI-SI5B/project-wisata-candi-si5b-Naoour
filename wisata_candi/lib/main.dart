b88ed0126bbae3b20fdcad627974d467a4c6e085
Mustofa Akhyar <148296892+Naoour@users.noreply.github.com>
2024-11-11 10:34:40 +0700

    commit pertama

---
+++ wisata_candi/lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:wisata_candi/screens/detail.screen.dart';
+
+void main() {
+  runApp(const MainApp());
+}
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'wisata candi',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: Detailscreen(),
+    );
+  }
+}
